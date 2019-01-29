cff8b2fb81a880073658964024a9b0840bdfb01e
Zsolt Üveges <zsolt.uveges93@gmail.com>
2019-01-29 19:51:35 +0100

    Generate car form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarFormComponent } from './car-form/car-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarFormComponent,
   ],
   imports: [
     BrowserModule
