a9c6bffa2b806631f9714b64bcdd3f6aa4104ed4
Zsolt Üveges <zsolt.uveges93@gmail.com>
2019-01-30 00:45:21 +0100

    Hide the save button until the form is not valid

--- src/app/car-form/car-form.component.ts
+++ src/app/car-form/car-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {CarFormService} from "../services/carForm.service";
 
 @Component({
@@ -15,8 +15,8 @@ export class CarFormComponent implements OnInit {
 
   ngOnInit() {
     this.carForm = new FormGroup({
-      'brand': new FormControl(null),
-      'model': new FormControl(null),
+      'brand': new FormControl(null, [Validators.required]),
+      'model': new FormControl(null, [Validators.required]),
       'color': new FormControl(null),
       'buildDate': new FormControl(null)
     })
