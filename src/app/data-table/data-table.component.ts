8b3d1f3f22c2aa8c1fbf89e0ef05565b37564da0
Zsolt Üveges <zsolt.uveges93@gmail.com>
2019-01-29 22:40:23 +0100

    Getting cars from service and subscribe to changes

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {CarModel} from "../models/car.model";
+import {CarFormService} from "../services/carForm.service";
 
 @Component({
   selector: 'app-data-table',
@@ -10,9 +11,15 @@ export class DataTableComponent implements OnInit {
   displayedColumns: string[] = ['brand', 'model', 'color', 'buildDate'];
   cars: CarModel[];
 
-  constructor() { }
+  constructor(private carFormService: CarFormService) { }
 
   ngOnInit() {
+    this.cars = this.carFormService.getCars();
+    this.carFormService.carsChanged.subscribe(
+      (cars: CarModel[]) => {
+        this.cars = cars;
+      }
+    )
   }
 
 }
