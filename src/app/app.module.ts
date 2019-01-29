4cce9b11d1c5346913c20b3bebbaa80cc737fa32
Zsolt Üveges <zsolt.uveges93@gmail.com>
2019-01-29 20:00:06 +0100

    Generate data table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CarFormComponent } from './car-form/car-form.component';
+import { DataTableComponent } from './data-table/data-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarFormComponent,
+    DataTableComponent,
   ],
   imports: [
     BrowserModule
