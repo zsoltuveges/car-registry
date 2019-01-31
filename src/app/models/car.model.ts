32aa3bea2de1ab690bf1da7309c7d87191521c2b
Zsolt Üveges <zsolt.uveges93@gmail.com>
2019-01-31 14:39:40 +0100

    Change date format to firebase compatible

--- src/app/models/car.model.ts
+++ src/app/models/car.model.ts
@@ -2,10 +2,10 @@ export class CarModel {
   brand: string;
   model: string;
   color: string;
-  buildDate: DateTimeFormat;
+  buildDate: Date;
 
 
-  constructor(brand: string, model: string, color: string = null, buildDate: DateTimeFormat) {
+  constructor(brand: string, model: string, color: string = null, buildDate: Date) {
     this.brand = brand;
     this.model = model;
     this.color = color;
