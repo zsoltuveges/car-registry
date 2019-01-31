d9b7a21ca5dcdd0af0a1978983af286e2f3eb01d
Zsolt Üveges <zsolt.uveges93@gmail.com>
2019-01-31 14:39:08 +0100

    Send form to firebase

--- src/app/car-form/car-form.component.ts
+++ src/app/car-form/car-form.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {CarFormService} from "../services/carForm.service";
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {CarFormService} from '../services/carForm.service';
+import {FirebaseService} from '../services/firebase.service';
 
 @Component({
   selector: 'app-car-form',
@@ -10,7 +11,8 @@ import {CarFormService} from "../services/carForm.service";
 export class CarFormComponent implements OnInit {
   carForm: FormGroup;
 
-  constructor(private carFormService: CarFormService) {
+  constructor(private carFormService: CarFormService,
+              private firebaseService: FirebaseService) {
   }
 
   ngOnInit() {
@@ -19,12 +21,17 @@ export class CarFormComponent implements OnInit {
       'model': new FormControl(null, [Validators.required]),
       'color': new FormControl(null),
       'buildDate': new FormControl(null)
-    })
+    });
   }
 
-  onSubmit() {
+  onSubmitToMemory() {
     this.carFormService.addCar(this.carForm['value']);
     this.carForm.reset();
+  }
+
+  onSubmitToFirebase() {
+    this.carForm['value'].buildDate = new Date(this.carForm['value'].buildDate.toDateString()).toDateString();
+    this.firebaseService.addCar(this.carForm['value']);
   }
 
 }
