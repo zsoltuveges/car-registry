d0c2fce7bc9b667db62a4b3531cf2a1ea801de61
Zsolt Üveges <zsolt.uveges93@gmail.com>
2019-01-31 17:10:59 +0100

    Add variable to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB6EIEGEGX0OCADJzvpYq_-xSZtmU5uFHg',
+    authDomain: 'autonyilvantarto.firebaseapp.com',
+    databaseURL: 'https://autonyilvantarto.firebaseio.com',
+    projectId: 'autonyilvantarto',
+    storageBucket: 'autonyilvantarto.appspot.com',
+    messagingSenderId: '92391634963'
+  }
 };
